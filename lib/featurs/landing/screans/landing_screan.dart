54302ccf45dbd07be2ca3e64d29078446d38ff23
bero9 <berohamood545@gmail.com>
2023-11-29 19:30:09 +0300

    add new file and edit to landing_screan.dart

--- lib/featurs/landing/screans/landing_screan.dart
+++ lib/featurs/landing/screans/landing_screan.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsappclone/colors.dart';
+
+import '../../../common/widgets/custom_button.dart';
 class landinScreean extends StatelessWidget {
   const landinScreean({super.key});
 
@@ -9,20 +11,31 @@ class landinScreean extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     return  Scaffold(
       body: SafeArea(
+        child: Center(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
+            // crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               const SizedBox(height: 50,),
-              const Text("Welcome To Bero WhatsApp",style: TextStyle(
+              const Text("Welcome To  WhatsApp",style: TextStyle(
                 fontSize: 28,
                 fontWeight: FontWeight.w600,
-
+        
               ),),
                SizedBox(height:size.height/9),
-              Image.asset("assets/bg.png",height: 300,width: 300,color: tabColor,),
+              Image.asset("assets/bg.png",height: 340,width: 340,color: tabColor,),
+              SizedBox(height:size.height/9),
+              const  Padding(
+                padding: EdgeInsets.all( 15.0),
+                child:  Text("Read our Privacy policy.Tap'Agree and Continue'to accept the term of services"
+                ,style: TextStyle(
+                    color: Colors.grey,),
+                textAlign: TextAlign.center,),
+              ),
+              const SizedBox(height: 10,),
+              CustomButton(text: "Agree And Continue", onPressed: (){})
             ],
           ),
+        ),
       ),
     );
   }
